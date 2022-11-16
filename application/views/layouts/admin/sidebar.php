e7a65abdb9b852b4b270ba19bf52a98e3e22e7d9
hendi234 <hendi.satrio03@gmail.com>
2022-11-16 18:11:19 +0700

    fix conflict on databases

--- application/views/layouts/admin/sidebar.php
+++ application/views/layouts/admin/sidebar.php
@@ -1,13 +1,7 @@
 <div class="sidebar">
-    <ul>
-        <li onclick="window.location.href = '<?= base_url('article') ?>'">daftar artikel</li>
-<<<<<<< HEAD
-        <li onclick="window.location.href = '<?= base_url('article/create_article') ?>'">create artikel</li>
-        <li onclick="window.location.href = '<?= base_url('galery') ?>'">galery</li>
-=======
-        <li>users</li>
-        <li onclick="window.location.href = '<?= base_url('article/create_article') ?>'">create artikel</li>
-        <li onclick="window.location.href = '<?= base_url('admin/galery') ?>'">galery</li>
->>>>>>> 20311f8add24159f14a03875cc78ba44d60d9315
-    </ul>
+        <ul>
+                <li onclick="window.location.href = '<?= base_url('article') ?>'">daftar artikel</li>
+                <li onclick="window.location.href = '<?= base_url('article/create_article') ?>'">create artikel</li>
+                <li onclick="window.location.href = '<?= base_url('galery') ?>'">galery</li>
+        </ul>
 </div>
