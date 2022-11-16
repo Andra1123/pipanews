e14fb2a2927ce40f7fd4135a5a1763c2130db53b
andra1123 <andralutfiana20@gmail.com>
2022-11-16 20:07:35 +0700

    memperbaiki laman daftar aricle

--- application/views/article/index.php
+++ application/views/article/index.php
@@ -1,18 +1,23 @@
-<div class="text-center">
-    <a href="<?= base_url('article/create_article') ?>" class="btn btn-success">Tambah Artikel</a>
-</div>
-
-<div class="row justify-content-center">
-    <?php foreach ($articles as $article) : ?>
-        <div class="col-4">
-            <div class="card" style="width: 18rem;">
-                <img src="<?= base_url('assets/images/artikel/' . $article->image) ?>" class="card-img-top" onclick="window.location.href = '<?= base_url('article/show_article/' . $article->id_article) ?>'">
-                <div class="card-body">
-                    <h5 class="card-title"><?= $article->title ?></h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+<?php $this->load->view('layouts/admin/navbar') ?>
+<div class="d-flex">
+    <?php $this->load->view('layouts/admin/sidebar') ?>
+    <div class="mainbar"> 
+        <div class="row justify-content-center">
+            <?php foreach ($articles as $article) : ?>
+                <div class="col-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="<?= base_url('assets/images/artikel/' . $article->image) ?>" class="card-img-top" onclick="window.location.href = '<?= base_url('article/show_article/' . $article->id_article) ?>'">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $article->title ?></h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            <?php endforeach ?>
         </div>
-    <?php endforeach ?>
+        <div class="text-center">
+            <a href="<?= base_url('article/create_article') ?>" class="btn btn-success">Tambah Artikel</a>
+        </div>
+    </div>
 </div>
